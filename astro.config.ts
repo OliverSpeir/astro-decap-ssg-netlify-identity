ab6fb07f9ac1da965fac9874c34803b05e884566
Oliver Speir <oliverspeir9@gmail.com>
2023-12-17 23:17:05 -0700

    public folder being set prevents need for remark plugin

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 import type { AstroUserConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import images from "./src/assets/utils/markdown-plugins/images";
-
 const config: AstroUserConfig = defineConfig({
 	site: "http://www.example.com",
 	integrations: [
@@ -11,9 +9,6 @@ const config: AstroUserConfig = defineConfig({
 			applyBaseStyles: false,
 		}),
 	],
-	markdown: {
-		remarkPlugins: [images],
-	},
 });
 
 // https://astro.build/config
